1b2b538ee34ebbea6d6234ab93c00c72c08e058c
Albert Fernandez <albert.fernandez@qubit.com>
2020-03-30 12:07:44 +0200

    Added start script

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var app = require('express')
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
   res.send('Hello world!')
 })
 
-app.listen(3000)
+app.listen(port)
