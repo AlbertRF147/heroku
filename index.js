6441663dfcac48bc690ae3347b5a741965e1b43b
Albert Fernandez <albert.fernandez@qubit.com>
2020-03-30 13:44:19 +0200

    added static file css

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 5000
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
 })
